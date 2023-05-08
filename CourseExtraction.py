de2ca0afa3f1ba18783885bf512f33eba1642e44
Michael Matar <mike.matar97@gmail.com>
2023-05-08 21:30:20 +0300

    passing login page

--- CourseExtraction.py
+++ CourseExtraction.py
@@ -11,3 +11,15 @@ password = sys.argv[2]
 path="C:\\Users\\miche\\Downloads\\chromedriver_win32\\chromedriver.exe"
 service= Service(executable_path=path)
 driver=webdriver.Chrome(service=service)
+url="https://myportal.lau.edu.lb/Pages/studentPortal.aspx"
+driver.get(url)
+
+username_input=driver.find_element("xpath","//input[@type='text' and @id='username' and @name='username']")
+
+password_input=driver.find_element("xpath","//input[@type='password' and @id='password']")
+
+button_input=driver.find_element("xpath","//input[@type='submit' and @value='Log In']")
+
+username_input.send_keys(username)
+password_input.send_keys(password)
+button_input.click()
